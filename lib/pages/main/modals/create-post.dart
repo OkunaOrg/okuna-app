37f4aa0a3f2d637ac13841c4db611df26ecf9073
Joel Hernandez <joel@open-book.org>
2018-11-07 18:29:40 +0100

    :construction: layout of create post modal

--- lib/pages/main/modals/create-post.dart
+++ lib/pages/main/modals/create-post.dart
@@ -1,17 +1,33 @@
-
+import 'package:Openbook/models/user.dart';
+import 'package:Openbook/provider.dart';
+import 'package:Openbook/services/user.dart';
 import 'package:Openbook/widgets/buttons/primary-button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CreatePostModal extends StatelessWidget{
+class CreatePostModal extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return CreatePostModalState();
+  }
+}
+
+class CreatePostModalState extends State<CreatePostModal> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    var openbookProvider = OpenbookProvider.of(context);
+    var userService = openbookProvider.userService;
+
     return Material(
       child: CupertinoPageScaffold(
           navigationBar: CupertinoNavigationBar(
             leading: GestureDetector(
-              child: Icon(Icons.close),
+              child: Icon(Icons.close, color: Colors.black87),
               onTap: () {
                 Navigator.pop(context);
               },
@@ -23,9 +39,85 @@ class CreatePostModal extends StatelessWidget{
               child: Text('Post'),
             ),
           ),
-          child: Center(
-            child: Text('Communities Page Content'),
-          )),
+          child: SafeArea(
+              child: Container(
+                  padding: EdgeInsets.only(top: 20.0, left: 20.0),
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.black12,
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              height: 39.0,
+                              width: 39.0,
+                              child: Container(
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  child: _buildUserAvatar(userService),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 20.0,
+                            ),
+                            Expanded(
+                                child: SingleChildScrollView(
+                              child: Column(
+                                children: <Widget>[
+                                  TextField(
+                                    keyboardType: TextInputType.multiline,
+                                    maxLines: 5,
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: 'What\'s up?'),
+                                    autocorrect: true,
+                                  ),
+                                ],
+                              ),
+                            ))
+                          ],
+                        ),
+                      ),
+                      Container(
+                        color: Colors.red,
+                        padding: EdgeInsets.only(left: 20.0, bottom: 20.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text('A'),
+                            Text('B'),
+                          ],
+                        ),
+                      )
+                    ],
+                  )))),
+    );
+  }
+
+  Widget _buildUserAvatar(UserService userService) {
+    return StreamBuilder(
+      stream: userService.loggedInUserChange,
+      initialData: null,
+      builder: (context, AsyncSnapshot<User> snapshot) {
+        var user = snapshot.data;
+
+        var avatar;
+
+        if (user == null) {
+          avatar = AssetImage('assets/images/avatar.png');
+        } else {
+          avatar = NetworkImage(user.profile.avatar);
+        }
+
+        return Container(
+          child: null,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: avatar, fit: BoxFit.cover)),
+        );
+      },
     );
   }
 }
