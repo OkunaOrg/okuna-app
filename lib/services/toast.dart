fc66d21e9e69f8741f0d62fe27a71997d9c6d924
Joel Hernandez <joel@open-book.org>
2018-11-11 12:13:37 +0100

    :construction: add custom color per toast type

--- lib/services/toast.dart
+++ lib/services/toast.dart
@@ -1,6 +1,7 @@
 import 'package:Openbook/widgets/icon.dart';
 import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
+import 'package:pigment/pigment.dart';
 
 enum ToastType { info, warning, success, error }
 
@@ -54,20 +55,24 @@ class ToastService {
       @required BuildContext context,
       @required ToastType type}) {
     Widget icon;
-
+    Color backgroundColor;
 
     switch (type) {
       case ToastType.warning:
         icon = OBIcon(OBIcons.warning);
+        backgroundColor = Pigment.fromString('#EFB500');
         break;
       case ToastType.info:
         icon = OBIcon(OBIcons.info);
+        backgroundColor = Pigment.fromString('#008BEF');
         break;
       case ToastType.success:
         icon = OBIcon(OBIcons.success);
+        backgroundColor = Pigment.fromString('#7FD421');
         break;
       case ToastType.error:
         icon = OBIcon(OBIcons.error);
+        backgroundColor = Pigment.fromString('#D42140');
         break;
       default:
         throw 'Unsupported ToastType';
@@ -78,9 +83,15 @@ class ToastService {
 /*    var toast = Flushbar(
       title: title,
       message: message,
-      duration: toastDuration
+      duration: toastDuration,
+      backgroundColor: backgroundColor,
+      //borderRadius: 500.0,
+      //aroundPadding: 60.0,
+      flushbarPosition: FlushbarPosition.BOTTOM,
     );
 
-    toast.show(context);*/
+    toast.show(context);
+
+    */
   }
 }
