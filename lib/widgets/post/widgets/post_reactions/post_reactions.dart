57b5dc22d83a219a10bf6518acf119de458fa231
Joel Hernandez <joel@open-book.org>
2018-11-21 17:41:59 +0100

    :construction: prettify post_reactions

--- lib/widgets/post/widgets/post_reactions/post_reactions.dart
+++ lib/widgets/post/widgets/post_reactions/post_reactions.dart
@@ -1,12 +1,16 @@
+import 'package:Openbook/models/emoji.dart';
 import 'package:Openbook/models/post.dart';
+import 'package:Openbook/models/post_reaction.dart';
 import 'package:Openbook/models/post_reactions_emoji_count.dart';
 import 'package:Openbook/widgets/post/widgets/post_reactions/widgets/reaction_emoji_count.dart';
 import 'package:flutter/material.dart';
 
 class OBPostReactions extends StatefulWidget {
   final Post post;
+  final OnWantsToReact onWantsToReact;
+  final OnWantsToUnReact onWantsToUnReact;
 
-  OBPostReactions(this.post);
+  OBPostReactions(this.post, {this.onWantsToReact, this.onWantsToUnReact});
 
   @override
   OBPostReactionsState createState() {
@@ -52,37 +56,60 @@ class OBPostReactionsState extends State<OBPostReactions> {
   }
 
   void _onEmojiReactionCountPressed(
-      PostReactionsEmojiCount postReactionsEmojiCount) {
-    setState(() {
-      if (postReactionsEmojiCount.reacted) {
-        // Remove reaction
-        if (postReactionsEmojiCount.count > 1) {
-          // There was more than one reaction. Minus one it.
-          var newEmojiCount = postReactionsEmojiCount.copy(
-              newReacted: false, newCount: postReactionsEmojiCount.count - 1);
-          int index = _emojiCounts.indexOf(postReactionsEmojiCount);
-          _emojiCounts[index] = newEmojiCount;
-        } else {
-          _emojiCounts.remove(postReactionsEmojiCount);
-        }
+      PostReactionsEmojiCount pressedEmojiCount) async {
+    if (pressedEmojiCount.reacted) {
+      await widget.onWantsToUnReact();
+      // Remove reaction
+      if (pressedEmojiCount.count > 1) {
+        // There was more than one reaction. Minus one it.
+        var newEmojiCount = pressedEmojiCount.copy(
+            newReacted: false, newCount: pressedEmojiCount.count - 1);
+        _replaceEmojiCount(pressedEmojiCount, newEmojiCount);
       } else {
-        // React
-        // Wants to react this
-        _emojiCounts = _emojiCounts.map((emojiCount) {
-          bool isPressedEmojiCount = emojiCount == postReactionsEmojiCount;
-          bool reacted = isPressedEmojiCount;
-          int count;
-          if (emojiCount.reacted) {
-            // If was reacted, decrement
-            count = emojiCount.count - 1;
-          } else {
-            // If its the pressed one, add one, else do nothing
-            count =
-                isPressedEmojiCount ? emojiCount.count + 1 : emojiCount.count;
-          }
-          return emojiCount.copy(newReacted: reacted, newCount: count);
-        }).toList();
+        _removeEmojiCount(pressedEmojiCount);
       }
+    } else {
+      // React
+      await widget.onWantsToReact(pressedEmojiCount.emoji);
+      // Wants to react this
+      var emojiCounts = _emojiCounts.map((emojiCount) {
+        bool isPressedEmojiCount = emojiCount == pressedEmojiCount;
+        bool reacted = isPressedEmojiCount;
+        int count;
+        if (emojiCount.reacted) {
+          // If was reacted, decrement
+          count = emojiCount.count - 1;
+        } else {
+          // If its the pressed one, add one, else do nothing
+          count = isPressedEmojiCount ? emojiCount.count + 1 : emojiCount.count;
+        }
+        return emojiCount.copy(newReacted: reacted, newCount: count);
+      }).toList();
+
+      _setEmojiCounts(emojiCounts);
+    }
+  }
+
+  void _replaceEmojiCount(emojiCount, newEmojiCount) {
+    setState(() {
+      int index = _emojiCounts.indexOf(emojiCount);
+      _emojiCounts[index] = newEmojiCount;
+    });
+  }
+
+  void _removeEmojiCount(emojiCount) {
+    setState(() {
+      _emojiCounts.remove(emojiCount);
+    });
+  }
+
+  void _setEmojiCounts(emojiCounts) {
+    setState(() {
+      _emojiCounts = emojiCounts;
     });
   }
 }
+
+typedef Future<PostReaction> OnWantsToReact(Emoji emoji);
+
+typedef Future<PostReaction> OnWantsToUnReact();
