f855f2aacad13a9efccdf8c1e8f1b4bd760ab8d5
Joel Hernandez <joel@open-book.org>
2018-11-25 19:34:56 +0100

    :sparkles: open user profile url

--- lib/pages/home/pages/profile/widgets/profile_card/widgets/profile_url.dart
+++ lib/pages/home/pages/profile/widgets/profile_card/widgets/profile_url.dart
@@ -1,5 +1,6 @@
 import 'package:Openbook/models/user.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class OBProfileUrl extends StatelessWidget {
   User user;
@@ -16,27 +17,38 @@ class OBProfileUrl extends StatelessWidget {
 
     Color color = Colors.black45;
 
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Icon(
-          Icons.link,
-          size: 14,
-          color: color,
-        ),
-        SizedBox(
-          width: 10,
-        ),
-        Flexible(
-            child: Text(
-              url,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(color: color),
-            )
-        )
-      ],
+    return GestureDetector(
+      onTap: _launchURL,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.link,
+            size: 14,
+            color: color,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Flexible(
+              child: Text(
+            url,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(color: color, decoration: TextDecoration.underline),
+          ))
+        ],
+      ),
     );
+  }
+
+  _launchURL() async {
+    String url = user.getProfileUrl();
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
