dd9d16714ede4197b7a23095530fa0ced5f02ef4
Joel Hernandez <joel@open-book.org>
2018-12-05 20:35:27 +0100

    :children_crossing: change page transition curve

--- lib/widgets/routes/slide_right_route.dart
+++ lib/widgets/routes/slide_right_route.dart
@@ -4,32 +4,34 @@ import 'package:flutter/material.dart';
 class OBSlideRightRoute<T> extends PageRouteBuilder<T> {
   final Widget widget;
   final Key key;
+
   OBSlideRightRoute({this.widget, this.key})
       : super(
-      opaque: false,
-      transitionDuration: const Duration(milliseconds: 200),
-      pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
-        return Material(
-            color: Color.fromARGB(0, 0, 0, 0),
-            child: Dismissible(
-                background: Container(color: Color.fromARGB(0, 0, 0, 0)),
-                direction: DismissDirection.startToEnd,
-                onDismissed: (direction) {
-                  Navigator.pop(context);
-                },
-                key: key,
-                child: widget
-                )
-            );
-      },
-      transitionsBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
-        return SlideTransition(
-            position: new Tween<Offset>(
-              begin: const Offset(1.0, 0.0),
-              end: Offset.zero,
-            ).animate(animation),
-            child: child
-        );
-      }
-  );
+            opaque: false,
+            transitionDuration: const Duration(milliseconds: 200),
+            pageBuilder: (BuildContext context, Animation<double> animation,
+                Animation<double> secondaryAnimation) {
+              return Material(
+                  color: Color.fromARGB(0, 0, 0, 0),
+                  child: Dismissible(
+                      background: Container(color: Color.fromARGB(0, 0, 0, 0)),
+                      direction: DismissDirection.startToEnd,
+                      onDismissed: (direction) {
+                        Navigator.pop(context);
+                      },
+                      key: key,
+                      child: widget));
+            },
+            transitionsBuilder: (BuildContext context,
+                Animation<double> animation,
+                Animation<double> secondaryAnimation,
+                Widget child) {
+              return SlideTransition(
+                  position: new Tween<Offset>(
+                    begin: const Offset(1.0, 0.0),
+                    end: Offset.zero,
+                  ).animate(
+                      CurvedAnimation(parent: animation, curve: Curves.fastOutSlowIn)),
+                  child: child);
+            });
 }
