e49fe893e9ada07c348d60a6f2f1c43a981cfb0c
Joel Hernandez <joel@open-book.org>
2018-12-16 20:18:48 +0100

    :sparkles: show lists part of in profile

--- lib/pages/home/pages/profile/widgets/profile_card/widgets/profile_in_lists.dart
+++ lib/pages/home/pages/profile/widgets/profile_card/widgets/profile_in_lists.dart
@@ -1,13 +1,13 @@
-import 'package:Openbook/models/circle.dart';
+import 'package:Openbook/models/follows_list.dart';
 import 'package:Openbook/models/user.dart';
-import 'package:Openbook/widgets/circle_color_preview.dart';
+import 'package:Openbook/widgets/emoji_preview.dart';
 import 'package:Openbook/widgets/theming/text.dart';
 import 'package:flutter/material.dart';
 
-class OBProfileConnectedIn extends StatelessWidget {
+class OBProfileInLists extends StatelessWidget {
   final User user;
 
-  OBProfileConnectedIn(this.user);
+  OBProfileInLists(this.user);
 
   @override
   Widget build(BuildContext context) {
@@ -15,34 +15,29 @@ class OBProfileConnectedIn extends StatelessWidget {
       stream: user.updateSubject,
       builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
         var user = snapshot.data;
-        var connectedCircles = user?.connectedCircles?.circles;
-        bool isFullyConnected =
-            user?.isFullyConnected != null && user.isFullyConnected;
-
-        if (connectedCircles == null ||
-            connectedCircles.length == 0 ||
-            !isFullyConnected) return SizedBox();
+        if (!user.hasFollowLists()) return SizedBox();
+        var followsLists = user.followLists.lists;
 
         List<Widget> connectionItems = [
           OBText(
-            'In circles',
+            'In lists',
             size: OBTextSize.small,
           )
         ];
 
-        connectedCircles.forEach((Circle circle) {
+        followsLists.forEach((FollowsList followsList) {
           connectionItems.add(Row(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              OBCircleColorPreview(
-                circle,
-                size: OBCircleColorPreviewSize.extraSmall,
+              OBEmojiPreview(
+                followsList.emoji,
+                size: OBEmojiPreviewSize.extraSmall,
               ),
               SizedBox(
                 width: 5,
               ),
               OBText(
-                circle.name,
+                followsList.name,
                 size: OBTextSize.small,
               )
             ],
