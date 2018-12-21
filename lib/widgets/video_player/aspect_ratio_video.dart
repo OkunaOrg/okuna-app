6107be62c93f0164d1130b9594fccb5b98c46711
Shantanu <shantanu.tarey@kpn.com>
2018-12-21 17:07:24 +0100

    🔧 added video_player plugin n ios / android configs

---
+++ lib/widgets/video_player/aspect_ratio_video.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class AspectRatioVideo extends StatefulWidget {
+  AspectRatioVideo(this.controller);
+
+  final VideoPlayerController controller;
+
+  @override
+  AspectRatioVideoState createState() => AspectRatioVideoState();
+}
+
+class AspectRatioVideoState extends State<AspectRatioVideo> {
+  VideoPlayerController get controller => widget.controller;
+  bool initialized = false;
+
+  VoidCallback listener;
+
+  @override
+  void initState() {
+    super.initState();
+    listener = () {
+      if (!mounted) {
+        return;
+      }
+      if (initialized != controller.value.initialized) {
+        initialized = controller.value.initialized;
+        setState(() {});
+      }
+    };
+    controller.addListener(listener);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (initialized) {
+      return Center(
+        child: AspectRatio(
+          aspectRatio: controller.value.size.width / controller.value.size.height,
+          child: VideoPlayer(controller),
+        ),
+      );
+    } else {
+      return Container();
+    }
+  }
+}
