823a5cbe4e6e4991b5766bdca3e43d46145a3a12
Joel Hernandez <joel@open-book.org>
2019-02-14 11:22:42 +0100

    :sparkles: open moderators user profile

--- lib/pages/home/pages/community/widgets/community_moderators.dart
+++ lib/pages/home/pages/community/widgets/community_moderators.dart
@@ -1,6 +1,8 @@
 import 'package:Openbook/models/community.dart';
 import 'package:Openbook/models/user.dart';
 import 'package:Openbook/models/users_list.dart';
+import 'package:Openbook/provider.dart';
+import 'package:Openbook/services/navigation_service.dart';
 import 'package:Openbook/widgets/theming/text.dart';
 import 'package:Openbook/widgets/tiles/user_tile.dart';
 import 'package:flutter/material.dart';
@@ -44,7 +46,15 @@ class OBCommunityModerators extends StatelessWidget {
                     shrinkWrap: true,
                     children:
                         communityModerators.map((User communityModerator) {
-                      return OBUserTile(communityModerator);
+                      return OBUserTile(
+                        communityModerator,
+                        onUserTilePressed: (User user) {
+                          NavigationService navigationService =
+                              OpenbookProvider.of(context).navigationService;
+                          navigationService.navigateToUserProfile(
+                              user: communityModerator, context: context);
+                        },
+                      );
                     }).toList(),
                   )
                 ],
