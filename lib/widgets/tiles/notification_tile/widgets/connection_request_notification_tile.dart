cc4e0bb8570ef1804bcfcf8c93ae2c2f19ea4692
Joel Hernandez <joel@open-book.org>
2019-03-04 17:26:56 +0100

    :sparkles: add connection request notification

--- lib/widgets/tiles/notification_tile/widgets/connection_request_notification_tile.dart
+++ lib/widgets/tiles/notification_tile/widgets/connection_request_notification_tile.dart
@@ -1,5 +1,8 @@
 import 'package:Openbook/models/notifications/connection_request_notification.dart';
 import 'package:Openbook/models/notifications/notification.dart';
+import 'package:Openbook/provider.dart';
+import 'package:Openbook/widgets/avatars/avatar.dart';
+import 'package:Openbook/widgets/theming/rich_text.dart';
 import 'package:flutter/material.dart';
 
 class OBConnectionRequestNotificationTile extends StatelessWidget {
@@ -14,6 +17,29 @@ class OBConnectionRequestNotificationTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('Connection request');
+    String connectionRequesterUsername =
+        connectionRequestNotification.connectionRequester.username;
+    return ListTile(
+      onTap: () {
+        OpenbookProviderState openbookProvider = OpenbookProvider.of(context);
+
+        openbookProvider.navigationService.navigateToUserProfile(
+            user: connectionRequestNotification.connectionRequester,
+            context: context);
+      },
+      leading: OBAvatar(
+        size: OBAvatarSize.medium,
+        avatarUrl: connectionRequestNotification.connectionRequester.getProfileAvatar(),
+      ),
+      title: OBRichText(
+        children: [
+          TextSpan(
+            text: '@$connectionRequesterUsername',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          TextSpan(text: ' wants to connect with you.')
+        ],
+      ),
+    );
   }
 }
