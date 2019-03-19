bdb24849aef363b4645e9ed7cecda4d523331d31
Shantanu <shantanu.tarey@kpn.com>
2019-03-19 20:45:34 +0100

    ✨ add handler for pwd reset token

---
+++ lib/services/universal_links/handlers/password_reset_link.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'package:Openbook/pages/auth/create_account/blocs/create_account.dart';
+import 'package:Openbook/provider.dart';
+import 'package:Openbook/services/universal_links/universal_links.dart';
+import 'package:flutter/material.dart';
+
+class PasswordResetLinkHandler extends UniversalLinkHandler {
+  static const String passwordResetVerifyLink = '/api/auth/password/verify';
+
+  @override
+  Future handle({BuildContext context, String link}) async{
+    if (link.indexOf(passwordResetVerifyLink) != -1) {
+      final token = getPasswordResetVerificationTokenFromLink(link);
+      OpenbookProviderState openbookProvider = OpenbookProvider.of(context);
+      CreateAccountBloc createAccountBloc = openbookProvider.createAccountBloc;
+      createAccountBloc.setPasswordResetToken(token);
+      Navigator.pushReplacementNamed(context, '/auth/set_new_password_step');
+    }
+  }
+
+  String getPasswordResetVerificationTokenFromLink(String link) {
+    final params = Uri.parse(link).queryParametersAll;
+    var token = '';
+    if (params.containsKey('token')) {
+      token = params['token'][0];
+    }
+    return token;
+  }
+}
