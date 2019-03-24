d9fd42eb2ea2a9494e0ed06b59c271641dafbb66
Joel Hernandez <joel@open-book.org>
2019-03-25 00:57:19 +0100

    :bug: add exif rotation fixer for android

--- lib/services/image_picker.dart
+++ lib/services/image_picker.dart
@@ -1,17 +1,13 @@
 import 'dart:io';
-import 'dart:ui';
-
-import 'package:Openbook/services/theme.dart';
-import 'package:Openbook/services/theme_value_parser.dart';
-import 'package:Openbook/services/utils_service.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_exif_rotation/flutter_exif_rotation.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:meta/meta.dart';
 export 'package:image_picker/image_picker.dart';
 
 class ImagePickerService {
-
   static const Map IMAGE_RATIOS = {
     OBImageType.avatar: {'x': 1.0, 'y': 1.0},
     OBImageType.cover: {'x': 16.0, 'y': 9.0}
@@ -25,6 +21,8 @@ class ImagePickerService {
     if (image == null) {
       return null;
     }
+
+    image = await _fixImageRotation(image);
 
     double ratioX =
         imageType != OBImageType.post ? IMAGE_RATIOS[imageType]['x'] : null;
@@ -46,6 +44,23 @@ class ImagePickerService {
     var video = await ImagePicker.pickVideo(source: source);
 
     return video;
+  }
+
+  Future<File> _fixImageRotation(File image) async {
+    // Needed for the exif fix
+    await _ensureHasExternalStoragePermission();
+
+    if (Platform.isAndroid)
+      image = await FlutterExifRotation.rotateImage(path: image.path);
+
+    return image;
+  }
+
+  Future _ensureHasExternalStoragePermission()async {
+    PermissionStatus permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.storage);
+    if(permission == PermissionStatus.granted) return;
+
+    await PermissionHandler().requestPermissions([PermissionGroup.storage]);
   }
 }
 
