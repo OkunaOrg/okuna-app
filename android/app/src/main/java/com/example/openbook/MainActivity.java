4258680e7633849cf3dc2d6ff7307bb3ff2fdc83
Sophie Tauchert <999eagle@999eagle.moe>
2019-03-30 12:49:41 +0100

    🚧 Decode image uri in android and convert to jpeg

--- android/app/src/main/java/com/example/openbook/MainActivity.java
+++ android/app/src/main/java/com/example/openbook/MainActivity.java
@@ -1,12 +1,26 @@
 package social.openbook.app;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
@@ -55,11 +69,13 @@ public class MainActivity extends FlutterActivity {
   }
 
   private void sendIntent(Intent intent) {
-    if (intent.getAction().equals(Intent.ACTION_SEND) && intent.getType().equals("image/*")) {
+    if (intent.getAction().equals(Intent.ACTION_SEND) && intent.getType().startsWith("image/")) {
       if (eventSink != null) {
         Map<String, String> args = new HashMap<>();
+        Uri uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        uri = copyImageToTempFile(uri);
         args.put("type", intent.getType());
-        args.put("path", intent.getParcelableExtra(Intent.EXTRA_STREAM).toString());
+        args.put("path", uri.toString());
         Log.i(getClass().getSimpleName(), "sending intent to flutter");
         eventSink.success(args);
       } else if (!streamCanceled && !intentBacklog.contains(intent)) {
@@ -68,6 +84,42 @@ public class MainActivity extends FlutterActivity {
       }
     } else {
       Log.i(getClass().getSimpleName(), "received useless intent action " + intent.getAction() + " type " + intent.getType() + ", not sending");
+    }
+  }
+
+  private Uri copyImageToTempFile(Uri imageUri) {
+    try {
+      InputStream inputStream;
+      if (imageUri.getScheme().equals("content")) {
+        inputStream = this.getContentResolver().openInputStream(imageUri);
+      } else {
+        inputStream = new FileInputStream(new File(imageUri.getPath()));
+      }
+      Bitmap bmp = BitmapFactory.decodeStream(inputStream);
+      inputStream.close();
+      if (bmp == null) return null;
+
+      ByteArrayOutputStream imageDataStream = new ByteArrayOutputStream();
+      bmp.compress(Bitmap.CompressFormat.JPEG, 100, imageDataStream);
+
+      File imageFile = createTemporaryFile(".jpeg");
+      FileOutputStream fileOutput = new FileOutputStream(imageFile);
+      fileOutput.write(imageDataStream.toByteArray());
+      inputStream.close();
+      fileOutput.close();
+
+      return Uri.fromFile(imageFile);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private File createTemporaryFile(String extension) {
+    try {
+      String name = UUID.randomUUID().toString();
+      return File.createTempFile(name, extension, this.getExternalFilesDir(Environment.DIRECTORY_PICTURES));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
   }
 }
