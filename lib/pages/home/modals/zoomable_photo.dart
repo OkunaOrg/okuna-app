ee4e419eb210c91a50055cc482981dced5507edd
cyriana <cyriana@gmail.com>
2019-03-31 16:49:02 +0200

    :sparkles: changed the animation for dismissing the PhotoView. It has now a rotation when sliding out.

--- lib/pages/home/modals/zoomable_photo.dart
+++ lib/pages/home/modals/zoomable_photo.dart
@@ -5,6 +5,7 @@ import 'package:flutter_advanced_networkimage/provider.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:pigment/pigment.dart';
 import 'package:swipedetector/swipedetector.dart';
+import "dart:math" show pi;
 
 class OBZoomablePhotoModal extends StatefulWidget {
   final String imageUrl;
@@ -22,27 +23,43 @@ class OBZoomablePhotoModalState extends State<OBZoomablePhotoModal>
   bool isDismissible;
   AnimationController controller;
   Animation<Offset> offset;
+  Animation<double> rotation;
+  double angle = 0.0;
+  Offset off = new Offset(0, 0);
 
   @override
   void initState() {
     super.initState();
     controller =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
-    offset = Tween<Offset>(begin: Offset.zero, end: Offset(-1.0, 0.0))
+        AnimationController(vsync: this, duration: Duration(milliseconds: 600));
+
+    offset = Tween<Offset>(begin: Offset.zero, end: Offset(-800, 0)).chain(CurveTween(curve: Curves.easeInOutSine))
         .animate(controller);
+
+    rotation = Tween<double>(begin: 0.0, end: 2 *pi).chain(CurveTween(curve: Curves.easeInOutCubic)).animate(controller)
+      ..addListener(() {
+        angle = rotation.value;
+        off = offset.value;
+        setState(() {});
+      });
+
     isDismissible = true;
   }
 
   @override
   Widget build(BuildContext context) {
+    debugPrint("angle: $angle");
+    debugPrint("offset: $off");
     return WillPopScope(
       child: OBCupertinoPageScaffold(
           backgroundColor: Color.fromARGB(0, 0, 0, 0),
           child: Stack(
             children: <Widget>[
               SwipeDetector(
-                child: SlideTransition(
-                    position: offset,
+                child: Transform.translate(
+                  offset: off,
+                  child: Transform.rotate(
+                    angle: angle,
                     child: PhotoView(
                       backgroundDecoration:
                           BoxDecoration(color: Colors.transparent),
@@ -57,20 +74,22 @@ class OBZoomablePhotoModalState extends State<OBZoomablePhotoModal>
                               'assets/images/fallbacks/post-fallback.png'),
                       maxScale: PhotoViewComputedScale.covered,
                       minScale: PhotoViewComputedScale.contained,
-                    )),
+                    ),
+                  ),
+                ),
                 onSwipeUp: () {
                   setState(() {
                     offset = Tween<Offset>(
-                            begin: Offset.zero, end: Offset(0.0, -1.0))
+                            begin: Offset.zero, end: Offset(0.0, -800)).chain(CurveTween(curve: Curves.easeInOutSine))
                         .animate(controller);
                     _dismissModal();
                   });
                 },
                 onSwipeDown: () {
                   setState(() {
-                    offset =
-                        Tween<Offset>(begin: Offset.zero, end: Offset(0.0, 1.0))
-                            .animate(controller);
+                    offset = Tween<Offset>(
+                            begin: Offset.zero, end: Offset(0.0, 800)).chain(CurveTween(curve: Curves.easeInOutSine))
+                        .animate(controller);                                        
                     _dismissModal();
                   });
                 },
@@ -91,12 +110,12 @@ class OBZoomablePhotoModalState extends State<OBZoomablePhotoModal>
 
   Future _dismissModal() async {
     await controller.forward();
-    Navigator.pop(context, false);
+    Navigator.pop(context);
   }
 
   Future<bool> _dismissModalNoPop() async {
-    await controller.forward();
-    return true;
+    _dismissModal();
+    return false;
   }
 
   Widget _buildCloseButton() {
@@ -104,31 +123,27 @@ class OBZoomablePhotoModalState extends State<OBZoomablePhotoModal>
       bottom: 50,
       left: 0,
       right: 0,
-      child: AnimatedOpacity(
-        opacity: (isDismissible ? 1 : 0),
-        duration: Duration(milliseconds: 50),
-        child: SafeArea(
-            child: Column(
-          children: <Widget>[
-            GestureDetector(
-              onTapDown: (tap) {
-                _dismissModal();
-              },
-              child: Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Pigment.fromString('#1d1d1d'),
-                    borderRadius: BorderRadius.circular(50)),
-                child: const OBIcon(
-                  OBIcons.close,
-                  size: OBIconSize.large,
-                  color: Colors.white,
-                ),
+      child: SafeArea(
+          child: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTapDown: (tap) {
+              _dismissModal();
+            },
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: Pigment.fromString('#1d1d1d'),
+                  borderRadius: BorderRadius.circular(50)),
+              child: const OBIcon(
+                OBIcons.close,
+                size: OBIconSize.large,
+                color: Colors.white,
               ),
-            )
-          ],
-        )),
-      ),
+            ),
+          )
+        ],
+      )),
     );
   }
 
