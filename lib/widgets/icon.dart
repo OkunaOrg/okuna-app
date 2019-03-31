d26218341df65334d009fe28b067c238bfc7442c
Komposten <j.hjelm586@hotmail.com>
2019-03-31 19:48:14 +0200

    :construction: Tall photos are now cropped at 3/4 window height
    Also added an "expand" icon in the lower right corner of images. Needs a better design, and currently occurs on every image (i.e. not only cropped ones).

--- lib/widgets/icon.dart
+++ lib/widgets/icon.dart
@@ -191,6 +191,7 @@ class OBIcons {
   static const favoriteCommunity = OBIconData(nativeIcon: Icons.favorite);
   static const unfavoriteCommunity =
       OBIconData(nativeIcon: Icons.remove_circle);
+  static const fullscreen = OBIconData(nativeIcon: Icons.fullscreen);
   static const mutePost = OBIconData(nativeIcon: Icons.notifications_active);
   static const editPost = OBIconData(nativeIcon: Icons.edit);
   static const unmutePost = OBIconData(nativeIcon: Icons.notifications_off);
--- lib/widgets/post/widgets/post-body/widgets/post_body_image.dart
+++ lib/widgets/post/widgets/post-body/widgets/post_body_image.dart
@@ -1,6 +1,8 @@
+import 'package:Openbook/models/theme.dart';
 import 'package:Openbook/models/post.dart';
 import 'package:Openbook/provider.dart';
 import 'package:Openbook/widgets/progress_indicator.dart';
+import 'package:Openbook/widgets/icon.dart';
 import 'package:flutter_advanced_networkimage/provider.dart';
 import 'package:flutter_advanced_networkimage/transition.dart';
 import 'package:flutter/material.dart';
@@ -12,9 +14,16 @@ class OBPostBodyImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var provider = OpenbookProvider.of(context);
+    var themeService = provider.themeService;
+    var themeValueParserService = provider.themeValueParserService;
+
     String imageUrl = post.getImage();
     double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     double aspectRatio = post.getImageWidth() / post.getImageHeight();
+    double imageHeight = (screenWidth / aspectRatio);
+    double boxHeight = imageHeight < screenHeight*.75 ? imageHeight : screenHeight*.75;
 
     return GestureDetector(
       onTap: () {
@@ -24,25 +33,41 @@ class OBPostBodyImage extends StatelessWidget {
       },
       child: SizedBox(
         width: screenWidth,
-        height: screenWidth / aspectRatio,
-        child: TransitionToImage(
-          width: screenWidth,
-          height: screenWidth / aspectRatio,
-          fit: BoxFit.contain,
-          image: AdvancedNetworkImage(imageUrl,
-              useDiskCache: true,
-              fallbackAssetImage: 'assets/images/fallbacks/post-fallback.png',
-              retryLimit: 3,
-              timeoutDuration: const Duration(minutes: 1)),
-          // This is the default placeholder widget at loading status,
-          // you can write your own widget with CustomPainter.
-          placeholder: Center(
-            child: const OBProgressIndicator(),
+        height: boxHeight,
+        child: Stack(
+          children: <Widget>[TransitionToImage(
+            width: screenWidth,
+            height: imageHeight,
+            fit: BoxFit.fitWidth,
+            alignment: Alignment.topCenter,
+            image: AdvancedNetworkImage(imageUrl,
+                useDiskCache: true,
+                fallbackAssetImage: 'assets/images/fallbacks/post-fallback.png',
+                retryLimit: 3,
+                timeoutDuration: const Duration(minutes: 1)),
+            // This is the default placeholder widget at loading status,
+            // you can write your own widget with CustomPainter.
+            placeholder: Center(
+              child: const OBProgressIndicator(),
+            ),
+            // This is default duration
+            duration: const Duration(milliseconds: 300),
           ),
-          // This is default duration
-          duration: const Duration(milliseconds: 300),
+          Positioned(
+              bottom: 0,
+              right: 0,
+              child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: OBIcon(
+                    OBIcons.fullscreen,
+                    size: OBIconSize.extraLarge,
+                    themeColor: OBIconThemeColor.primaryAccent,
+                  )
+              )
+          ),
+          ],
         ),
-      ),
+      )
     );
   }
 }
