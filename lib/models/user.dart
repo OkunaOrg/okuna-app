f723260552c507b0b0ff5a51e2ccbd150421c784
Joel Hernandez <joel@open-book.org>
2019-04-04 18:15:08 +0200

    :recycle: community invites not parsed

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -309,6 +309,7 @@ class UserFactory extends UpdatableModelFactory<User> {
         connectedCircles: parseCircles(json['connected_circles']),
         communitiesMemberships:
             parseMemberships(json['communities_memberships']),
+        communitiesInvites: parseInvites(json['communities_invites']),
         followLists: parseFollowsLists(json['follow_lists']));
   }
 
