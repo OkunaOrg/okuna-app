70a09f7a74f05c3bc56b71fcbbf4e740a16ca758
Sophie Tauchert <999eagle@999eagle.moe>
2019-04-05 19:45:04 +0200

    :construction: Clean up, Change target

--- ios/OpenbookShareExtension/ShareViewController.h
+++ ios/OpenbookShareExtension/ShareViewController.h
@@ -1,11 +1,3 @@
-//
-//  ShareViewController.h
-//  OpenbookShareExtension
-//
-//  Created by Openbook on 05.04.19.
-//  Copyright © 2019 Openbook B.V. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import <Social/Social.h>
 
