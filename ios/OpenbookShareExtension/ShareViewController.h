634229108cc6ca85b4b825589f87279ef5779fd2
Sophie Tauchert <999eagle@999eagle.moe>
2019-04-05 18:00:30 +0200

    :construction: Add share extension

---
+++ ios/OpenbookShareExtension/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  OpenbookShareExtension
+//
+//  Created by Openbook on 05.04.19.
+//  Copyright © 2019 Openbook B.V. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
---
+++ ios/OpenbookShareExtension/ShareViewController.m
@@ -0,0 +1,34 @@
+//
+//  ShareViewController.m
+//  OpenbookShareExtension
+//
+//  Created by Openbook on 05.04.19.
+//  Copyright © 2019 Openbook B.V. All rights reserved.
+//
+
+#import "ShareViewController.h"
+
+@interface ShareViewController ()
+
+@end
+
+@implementation ShareViewController
+
+- (BOOL)isContentValid {
+    // Do validation of contentText and/or NSExtensionContext attachments here
+    return YES;
+}
+
+- (void)didSelectPost {
+    // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    
+    // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+    [self.extensionContext completeRequestReturningItems:@[] completionHandler:nil];
+}
+
+- (NSArray *)configurationItems {
+    // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+    return @[];
+}
+
+@end
