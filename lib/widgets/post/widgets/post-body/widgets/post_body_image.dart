a2c4de6c03e7a8f3c8335337bf5785d5553f2eae
Komposten <j.hjelm586@hotmail.com>
2019-04-13 19:50:22 +0200

    :lipstick: Changed the "expand image" visuals based on feedback

--- lib/widgets/post/widgets/post-body/widgets/post_body_image.dart
+++ lib/widgets/post/widgets/post-body/widgets/post_body_image.dart
@@ -1,4 +1,3 @@
-import 'package:Openbook/models/theme.dart';
 import 'package:Openbook/models/post.dart';
 import 'package:Openbook/provider.dart';
 import 'package:Openbook/widgets/progress_indicator.dart';
@@ -6,6 +5,8 @@ import 'package:Openbook/widgets/icon.dart';
 import 'package:flutter_advanced_networkimage/provider.dart';
 import 'package:flutter_advanced_networkimage/transition.dart';
 import 'package:flutter/material.dart';
+import 'package:pigment/pigment.dart';
+import 'dart:math';
 
 class OBPostBodyImage extends StatelessWidget {
   final Post post;
@@ -14,60 +15,59 @@ class OBPostBodyImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var provider = OpenbookProvider.of(context);
-    var themeService = provider.themeService;
-    var themeValueParserService = provider.themeValueParserService;
-
     String imageUrl = post.getImage();
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
+    double maxBoxHeight = screenHeight*.75;
+
     double aspectRatio = post.getImageWidth() / post.getImageHeight();
     double imageHeight = (screenWidth / aspectRatio);
-    double boxHeight = imageHeight < screenHeight*.75 ? imageHeight : screenHeight*.75;
+    double boxHeight = min(imageHeight, maxBoxHeight);
 
     return GestureDetector(
-      onTap: () {
-        var dialogService = OpenbookProvider.of(context).dialogService;
-        dialogService.showZoomablePhotoBoxView(
-            imageUrl: imageUrl, context: context);
-      },
-      child: SizedBox(
-        width: screenWidth,
-        height: boxHeight,
-        child: Stack(
-          children: <Widget>[TransitionToImage(
-            width: screenWidth,
-            height: imageHeight,
-            fit: BoxFit.fitWidth,
-            alignment: Alignment.topCenter,
-            image: AdvancedNetworkImage(imageUrl,
-                useDiskCache: true,
-                fallbackAssetImage: 'assets/images/fallbacks/post-fallback.png',
-                retryLimit: 3,
-                timeoutDuration: const Duration(minutes: 1)),
-            // This is the default placeholder widget at loading status,
-            // you can write your own widget with CustomPainter.
-            placeholder: Center(
-              child: const OBProgressIndicator(),
+        onTap: () {
+          var dialogService = OpenbookProvider.of(context).dialogService;
+          dialogService.showZoomablePhotoBoxView(
+              imageUrl: imageUrl, context: context);
+        },
+        child: SizedBox(
+          width: screenWidth,
+          height: boxHeight,
+          child: Stack(
+            children: <Widget>[TransitionToImage(
+              width: screenWidth,
+              height: imageHeight,
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.center,
+              image: AdvancedNetworkImage(imageUrl,
+                  useDiskCache: true,
+                  fallbackAssetImage: 'assets/images/fallbacks/post-fallback.png',
+                  retryLimit: 3,
+                  timeoutDuration: const Duration(minutes: 1)),
+              placeholder: Center(
+                child: const OBProgressIndicator(),
+              ),
+              duration: const Duration(milliseconds: 300),
             ),
-            // This is default duration
-            duration: const Duration(milliseconds: 300),
+            Positioned(
+                bottom: 10,
+                right: 10,
+                child: Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Pigment.fromString("#1d1d1d"), //Same as in OBZoomablePhotoModal
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                    child: OBIcon(
+                      OBIcons.fullscreen,
+                      size: OBIconSize.medium,
+                      color: Colors.white,
+                    )
+                )
+            ),
+            ],
           ),
-          Positioned(
-              bottom: 0,
-              right: 0,
-              child: Container(
-                  padding: EdgeInsets.all(10),
-                  child: OBIcon(
-                    OBIcons.fullscreen,
-                    size: OBIconSize.extraLarge,
-                    themeColor: OBIconThemeColor.primaryAccent,
-                  )
-              )
-          ),
-          ],
-        ),
-      )
+        )
     );
   }
 }
