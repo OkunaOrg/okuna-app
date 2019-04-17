e365b7b075e3d91f22e9d6708c7ce3b41320ad71
Sophie Tauchert <999eagle@999eagle.moe>
2019-04-17 21:00:15 +0200

    :green_apple: Implement permission plugin on iOS

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,12 +1,14 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 #import <uni_links/UniLinksPlugin.h>
+#import "Plugins/Permissions.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+  [Permissions registerWithRegistrar:[self registrarForPlugin:@"Permission"]];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
