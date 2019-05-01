363030023b9afad8eebf07b8f5ead3025be374d4
Shantanu <shantanutarey@gmail.com>
2019-05-01 20:24:57 +0200

    :recycle: use github pages url

--- lib/services/documents.dart
+++ lib/services/documents.dart
@@ -3,14 +3,21 @@ import 'package:Openbook/services/httpie.dart';
 class DocumentsService {
   HttpieService _httpService;
 
-  static const guidelinesUrl =
-      'https://raw.githubusercontent.com/OpenbookOrg/openbook-api/master/COMMUNITY_GUIDELINES.md';
+  static const guidelinesUrl = 'https://openbookorg.github.io/openbook-api/COMMUNITY_GUIDELINES.md';
 
   void setHttpService(HttpieService httpService) {
     _httpService = httpService;
   }
 
   Future<String> getCommunityGuidelines() async {
+    Map<String, String> headers = {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+      'Accept' :'text/plain; charset=utf-8',
+      'Cache-Control': 'no-cache',
+      'Connection': 'keep-alive',
+      'accept-encoding': 'gzip, deflate',
+    };
+
     HttpieResponse response = await _httpService.get(guidelinesUrl);
     return response.body;
   }
