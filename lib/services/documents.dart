074e0f472b44b9f52e70341d02cac4ffabd39381
Shantanu <shantanutarey@gmail.com>
2019-05-01 20:26:25 +0200

    :fire: remove headers

--- lib/services/documents.dart
+++ lib/services/documents.dart
@@ -10,14 +10,6 @@ class DocumentsService {
   }
 
   Future<String> getCommunityGuidelines() async {
-    Map<String, String> headers = {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
-      'Accept' :'text/plain; charset=utf-8',
-      'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive',
-      'accept-encoding': 'gzip, deflate',
-    };
-
     HttpieResponse response = await _httpService.get(guidelinesUrl);
     return response.body;
   }
