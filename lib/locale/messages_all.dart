6e1c10cc59f6c201e7b32e81459d09dd47f077c0
Shantanu <shantanutarey@gmail.com>
2019-08-01 16:41:16 +0200

    :globe_with_meridians: add compiled and source files for de and es.

--- lib/locale/messages_all.dart
+++ lib/locale/messages_all.dart
@@ -9,18 +9,28 @@ import 'package:intl/message_lookup_by_library.dart';
 // ignore: implementation_imports
 import 'package:intl/src/intl_helpers.dart';
 
+import 'messages_de.dart' as messages_de;
 import 'messages_en.dart' as messages_en;
+import 'messages_es.dart' as messages_es;
 
 typedef Future<dynamic> LibraryLoader();
 Map<String, LibraryLoader> _deferredLibraries = {
 // ignore: unnecessary_new
+  'de': () => new Future.value(null),
+// ignore: unnecessary_new
   'en': () => new Future.value(null),
+// ignore: unnecessary_new
+  'es': () => new Future.value(null),
 };
 
 MessageLookupByLibrary _findExact(localeName) {
   switch (localeName) {
+    case 'de':
+      return messages_de.messages;
     case 'en':
       return messages_en.messages;
+    case 'es':
+      return messages_es.messages;
     default:
       return null;
   }
