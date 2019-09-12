73b9df5af60905f0b6ab6d5c210c1a6a45e9ab77
Shantanu <shantanutarey@gmail.com>
2019-09-12 19:17:56 +0200

    :construction: explore posts

--- lib/pages/home/pages/search/widgets/trending_posts.dart
+++ lib/pages/home/pages/search/widgets/trending_posts.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:Okuna/models/post.dart';
 import 'package:Okuna/models/posts_list.dart';
+import 'package:Okuna/models/top_post.dart';
 import 'package:Okuna/provider.dart';
 import 'package:Okuna/services/httpie.dart';
 import 'package:Okuna/services/localization.dart';
@@ -30,10 +31,10 @@ class OBTrendingPosts extends StatefulWidget {
 class OBTrendingPostsState extends State<OBTrendingPosts> {
   UserService _userService;
   LocalizationService _localizationService;
-
   CancelableOperation _getTrendingPostsOperation;
-
   OBPostsStreamController _obPostsStreamController;
+
+  List<TopPost> _currentPosts = [];
 
   @override
   void initState() {
@@ -63,7 +64,7 @@ class OBTrendingPostsState extends State<OBTrendingPosts> {
     _localizationService = openbookProvider.localizationService;
 
     return OBPostsStream(
-      streamIdentifier: 'trendingPosts',
+      streamIdentifier: 'explorePostsTab',
       refresher: _postsStreamRefresher,
       onScrollLoader: _postsStreamOnScrollLoader,
       controller: _obPostsStreamController,
@@ -79,14 +80,42 @@ class OBTrendingPostsState extends State<OBTrendingPosts> {
   }
 
   Future<List<Post>> _postsStreamRefresher() async {
-    List<Post> posts = (await _userService.getTrendingPosts()).posts;
+    List<TopPost> topPosts = (await _userService.getTopPosts()).posts;
+    List<Post> posts = topPosts.map((topPost) => topPost.post).toList();
+    _setTopPosts(topPosts);
 
     return posts;
   }
 
   Future<List<Post>> _postsStreamOnScrollLoader(List<Post> posts) async {
-    return [];
+    TopPost lastTopPost = _currentPosts.last;
+    int lastTopPostId = lastTopPost.id;
+
+    List<TopPost> moreTopPosts = (await _userService.getTopPosts(
+        maxId: lastTopPostId,
+        count: 10))
+        .posts;
+
+    List<Post> morePosts = moreTopPosts.map((topPost) => topPost.post).toList();
+
+    _appendCurrentTopPosts(moreTopPosts);
+
+    return morePosts;
   }
+
+  void _setTopPosts(List<TopPost> posts) {
+    setState(() {
+      _currentPosts = posts;
+    });
+  }
+
+  void _appendCurrentTopPosts(List<TopPost> posts) {
+    List<TopPost> newPosts = _currentPosts + posts;
+    setState(() {
+      _currentPosts = newPosts;
+    });
+  }
+
 }
 
 class OBTrendingPostsController {
