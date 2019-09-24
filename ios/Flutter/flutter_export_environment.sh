6db4fc8d0ffd4b7dc178703df0cbfe7ddca275f2
Joel Hernandez <joel@open-book.org>
2019-09-24 12:09:27 +0200

    :bug: fix bug with file picking

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/lifenautjoe/Documents/code/libraries/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/lifenautjoe/Documents/code/okuna/okuna-app"
-export "FLUTTER_TARGET=/Users/lifenautjoe/Documents/code/okuna/okuna-app/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/lifenautjoe/Documents/code/libraries/flutter/bin/cache/artifacts/engine/ios-release"
