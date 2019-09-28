7d48f587c5d9ea933e52abf38d0a6a63e8df8619
Shantanu <shantanutarey@gmail.com>
2019-09-28 18:49:17 +0200

    :wrench: ios files

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/lifenautjoe/Documents/code/libraries/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/lifenautjoe/Documents/code/okuna/okuna-app"
-export "FLUTTER_TARGET=/Users/lifenautjoe/Documents/code/okuna/okuna-app/lib/main.dart"
+export "FLUTTER_ROOT=/Users/shantanu/Documents/Openbook/devtools/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/shantanu/Documents/Openbook/openbook-app"
+export "FLUTTER_TARGET=/Users/shantanu/Documents/Openbook/openbook-app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/lifenautjoe/Documents/code/libraries/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_FRAMEWORK_DIR=/Users/shantanu/Documents/Openbook/devtools/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.0.53"
 export "FLUTTER_BUILD_NUMBER=59"
+export "TRACK_WIDGET_CREATION=true"
