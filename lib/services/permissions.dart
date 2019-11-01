9a5582083d1cfe627d06e0df9f578ef7e5e8fda4
Joel Hernandez <joel@open-book.org>
2019-11-01 17:17:54 +0100

    :fire: remove permission request code as it does NOT work

--- lib/services/permissions.dart
+++ lib/services/permissions.dart
@@ -1,7 +1,6 @@
 import 'package:Okuna/services/localization.dart';
 import 'package:Okuna/services/toast.dart';
 import 'package:flutter/material.dart';
-import 'package:permission/permission.dart';
 
 class PermissionsService {
   ToastService _toastService;
@@ -18,7 +17,7 @@ class PermissionsService {
   Future<bool> requestStoragePermissions(
       {@required BuildContext context}) async {
     return _requestPermissionWithErrorMessage(
-        permission: PermissionName.Storage,
+        permission: null,
         errorMessage:
             _localizationService.permissions_service__storage_permission_denied,
         context: context);
@@ -27,28 +26,16 @@ class PermissionsService {
   Future<bool> requestCameraPermissions(
       {@required BuildContext context}) async {
     return _requestPermissionWithErrorMessage(
-        permission: PermissionName.Camera,
+        permission: null,
         errorMessage:
             _localizationService.permissions_service__camera_permission_denied,
         context: context);
   }
 
   Future<bool> _requestPermissionWithErrorMessage(
-      {@required PermissionName permission,
+      {@required dynamic permission,
       @required String errorMessage,
       @required BuildContext context}) async {
-    PermissionStatus permissionStatus =
-        await Permission.getSinglePermissionStatus(permission);
-    if (permissionStatus == PermissionStatus.notDecided) {
-      permissionStatus = await Permission.requestSinglePermission(permission);
-    }
-
-    if (permissionStatus == PermissionStatus.deny ||
-        permissionStatus == PermissionStatus.notAgain) {
-      _toastService.error(message: errorMessage, context: context);
-      return false;
-    }
-
     return true;
   }
 }
