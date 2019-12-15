d8887b2f92bf626d3c46e2b280c56ffe6abd639a
Joel Hernandez <joel@open-book.org>
2019-12-15 12:55:39 +0100

    :bug: fix hashtag color issue

--- lib/widgets/hashtag.dart
+++ lib/widgets/hashtag.dart
@@ -24,7 +24,7 @@ class OBHashtag extends StatelessWidget {
 
     if(textStyle != null) finalTextStyle = finalTextStyle.merge(textStyle);
 
-    Widget hashtagContent = Text(
+    Widget hashtagContent = OBText(
       '#' + (rawHashtagName ?? hashtag.name),
       style: finalTextStyle,
     );
