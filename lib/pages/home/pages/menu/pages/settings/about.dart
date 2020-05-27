695ca1eb930f8d78728bbe9f2df2ead9b1bd77f5
Joel Hernandez <joel@open-book.org>
2020-05-27 21:17:05 +0200

    :bookmark: up version again

--- lib/pages/home/pages/menu/pages/settings/about.dart
+++ lib/pages/home/pages/menu/pages/settings/about.dart
@@ -48,7 +48,7 @@ class OBAboutPageState extends State<OBAboutPage> {
             ListTile(
               leading: OBIcon(OBIcons.nativeInfo),
               title: OBText(
-                'Okuna v0.0.66'
+                'Okuna v0.0.67'
               ),
             ),
           ],
